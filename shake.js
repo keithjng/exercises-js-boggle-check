f4afbc4a6decefe2007ba8ec7dc4ee5a9149eb3f
Keith Ng <keith.jk.ng@gmail.com>
2020-02-03 16:11:30 -0800

    board with dice

--- shake.js
+++ shake.js
@@ -1,12 +1,40 @@
+let process = require('process');
+
+function print(str) {
+  process.stdout.write(str);
+}
+
 function shake() {
   let randLet = ''
-  let alphabet = 'abcdefghijklmnopqrstuvwxyz'
-  for (i = 0; i < 4; i++) {
-    for (j = 0; j < 4; j++) {
-      randLet = randLet + alphabet.charAt(Math.floor(Math.random() * 26))
+  let dice = [
+  'AAEEGN',
+  'ELRTTY',
+  'AOOTTW',
+  'ABBJOO',
+  'EHRTVW',
+  'CIMOTU',
+  'DISTTY',
+  'EIOSST',
+  'DELRVY',
+  'ACHOPS',
+  'HIMNQU',
+  'EEINSU',
+  'EEGHNW',
+  'AFFKPS',
+  'HLNNRZ',
+  'DEILRX',]
+  letCount = 0
+  for (i = 0; i < dice.length; i++) {
+    for (j = 0; j < 1; j++) {
+      randLet = randLet + dice[i].charAt(Math.floor(Math.random() * 6));
     }
-    console.log(randLet)
-    randLet = ''
+    print(randLet)
+    randLet = '';
+    letCount = letCount + 1;
+      if (letCount === 4) {
+        console.log();
+        letCount = 0;
+      }
   }
   return randLet
 }
